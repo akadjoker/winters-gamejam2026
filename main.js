b287a56884c667274482ac6fd28d4b41c5c14bbb
akadjoker <djokertheripper@gmail.com>
2026-02-22 08:16:11 +0000

    add preload

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpsfdoe1be.js
+// include: /tmp/tmp5yud5big.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -201,25 +201,25 @@ Module['FS_createPath']("/", "scripts", true, true);
     }
 
     }
-    loadPackage({"files": [{"filename": "/assets/crash.wav", "start": 0, "end": 4015}, {"filename": "/scripts/main.bu", "start": 4015, "end": 81378}], "remote_package_size": 81378});
+    loadPackage({"files": [{"filename": "/assets/crash.wav", "start": 0, "end": 4015}, {"filename": "/scripts/main.bu", "start": 4015, "end": 81839}], "remote_package_size": 81839});
 
   })();
 
-// end include: /tmp/tmpsfdoe1be.js
-// include: /tmp/tmp9_bl424l.js
+// end include: /tmp/tmp5yud5big.js
+// include: /tmp/tmpgy360sni.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmp9_bl424l.js
-// include: /tmp/tmpbq4q7b7t.js
+  // end include: /tmp/tmpgy360sni.js
+// include: /tmp/tmp0clb332o.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpbq4q7b7t.js
+  // end include: /tmp/tmp0clb332o.js
 
 
 var arguments_ = [];
